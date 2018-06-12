f3e637bbcafaa2171c6a8bddda61c12aef599493
amoorkie <40201499+amoorkie@users.noreply.github.com>
2018-06-12 20:03:59 +0300

    Add files via upload

---
+++ slider_files/owlcarousel/script_video.js
@@ -0,0 +1,10 @@
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        merge:true,
+        loop:true,
+        margin:10,
+        video:true,
+        lazyLoad:true,
+        center:true,
+       
+    })
