b59881e5b32a99e28dd361414d5e4ad26128e1e3
amoorkie <40201499+amoorkie@users.noreply.github.com>
2018-06-12 19:51:40 +0300

    Add files via upload

---
+++ nip/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	dots: true,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 4000,
+  	video: true,
+  	autoplayHoverPause: true
+  });
+});
---
+++ nip/js/script_slider_games.js
@@ -0,0 +1,8 @@
+$('.slider__games').owlCarousel({
+    items: 2,
+    loop: true,
+    video: true,
+    lazyLoad: true,
+    center: true,
+    nav: true,
+});
