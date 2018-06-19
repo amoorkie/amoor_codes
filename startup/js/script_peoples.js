f6543de8adaee7e7af46e6d41a9af8917d7b3840
amoorkie <40201499+amoorkie@users.noreply.github.com>
2018-06-19 17:31:56 +0300

    Add files via upload

---
+++ startup/js/script_peoples.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".about_us-slider").owlCarousel({
+  	items: 4,
+  	loop: true,
+  	dots: false,
+  	autoplay: false,
+  	nav: true,
+  	margin: 29
+  });
+});
---
+++ startup/js/script_reviews.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".reviews-slider").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: false,
+  	nav: false,
+  });
+});
